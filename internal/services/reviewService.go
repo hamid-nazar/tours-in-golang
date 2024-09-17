bc2b0f90177cc7720e85df9aeec1c52677db46d9
Hamid Nazari <nazarihamid490@gmail.com>
2024-09-17 12:55:54 +0200

    initial commit

--- internal/services/reviewService.go
+++ internal/services/reviewService.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hamid-nazari/tours-in-go/internal/models"
+)
+
+func CreateReview(ctx *gin.Context, review *models.Review) (*models.Review, error) {
+	return nil, nil
+}
